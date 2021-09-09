a723a1c8a24a0d74b16766ff76f91d1a9e356aa8
Jhon Jairo Hernandez <amonzos@protonmail.com>
2021-09-09 14:29:02 -0500

    update

--- src/index.js
+++ src/index.js
@@ -4,16 +4,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import colors from './colors';
 
+
+function Autor (props) {
+  return (
+    <div>
+      <span id="autor"> {props.autor} </span>
+      <a id="tweet-quote" href="https://twitter.com/" target="_blank" rel="noreferrer">
+        Tweeter
+      </a>
+    </div>
+  )
+}
 class Card extends React.Component {
   render () {
     return (
-      <div className="card-div" style={{backgroundColor: this.props.color}} >
-        <p>
+      <div id="quote-box" className="card-div" style={{backgroundColor: this.props.color}} >
+        <p id="text">
           {this.props.isquote && <span className="com">"</span>}
           {this.props.quote}
           {this.props.isquote && <span className="com">"</span> }
         </p>
-        <button className="btn" onClick={this.props.newQuote}> Nueva Frase</button>
+        {this.props.isquote && <Autor autor={this.props.autor} /> }
+        <button id="new-quote" className="btn" onClick={this.props.newQuote}>
+          Nueva Frase
+        </button>
       </div>
     )
   }
@@ -26,19 +40,21 @@ class App extends React.Component {
       idxColor: 1,
       quote: 'Hola, para iniciar presiona el boton',
       isquote: false,
+      autor: ''
     };
   }
-  
+
   newQuote = () => {
     axios.get('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand')      
       .then(response => response.data)
         .then(data => {
-          const randomQuote = Math.floor(Math.random() * 12);
-          const newIdx = Math.floor(Math.random() * 9);
+          const randomQuote = Math.floor(Math.random() * 10);
+          const newIdx = Math.floor(Math.random() * 12);
           this.setState({
             quote: data[randomQuote].content.rendered,
             isquote: true,
-            idxColor: newIdx
+            idxColor: newIdx,
+            autor: data[randomQuote].title.rendered,
           });
         });
   }
@@ -54,6 +70,7 @@ class App extends React.Component {
           newQuote={this.newQuote}
           isquote={this.state.isquote}
           quote={this.state.quote}
+          autor={this.state.autor}
         />
       </div>
     )
