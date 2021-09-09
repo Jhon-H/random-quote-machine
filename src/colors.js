199f9bc852061fdf8c08a03d3eb69f99be28f8c3
Jhon Jairo Hernandez <amonzos@protonmail.com>
2021-09-09 13:52:19 -0500

    funcionando, falta requisitos

---
+++ src/colors.js
@@ -0,0 +1,40 @@
+const colors = [
+ {
+  primary: "#202020",
+  second: "gray"
+ },
+ {
+  primary: "dodgerblue",
+  second: "deepskyblue"
+ },
+ {
+  primary: "palevioletred",
+  second: "#ff8282"
+ },
+ {
+  primary: "lightsalmon",
+  second: "moccasin"
+ },
+ {
+  primary: "orange",
+  second: "#ffcf75"
+ },
+ {
+  primary: "cadetblue",
+  second: "darkturquoise"
+ },
+ {
+  primary: "brown",
+  second: "crimson"
+ },
+ {
+  primary: "darkviolet",
+  second: "#ffcf75"
+ },
+ {
+  primary: "darkseagreen",
+  second: "#b8ffb8"
+ }
+]
+
+export default colors;
--- src/index.js
+++ src/index.js
@@ -2,32 +2,65 @@ import React from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
 import './index.css';
+import colors from './colors';
 
 class Card extends React.Component {
+  render () {
+    return (
+      <div className="card-div" style={{backgroundColor: this.props.color}} >
+        <p>
+          {this.props.isquote && <span className="com">"</span>}
+          {this.props.quote}
+          {this.props.isquote && <span className="com">"</span> }
+        </p>
+        <button className="btn" onClick={this.props.newQuote}> Nueva Frase</button>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
   constructor (props) {
     super(props);
-    this.state = { quote: '' };
+    this.state = {
+      idxColor: 0,
+      quote: 'Hola, para iniciar presiona el boton',
+      isquote: false,
+    };
   }
-
+  
   newQuote = () => {
-    axios.get('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand')
-      .then(response => response.content)
-        .then(response => {
-          this.setState({quote: response})
+    axios.get('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand')      
+      .then(response => response.data)
+        .then(data => {
+          const randomQuote = Math.floor(Math.random() * 10);
+          const newIdx = Math.floor(Math.random() * 9);
+          this.setState({
+            quote: data[randomQuote].content.rendered,
+            isquote: true,
+            idxColor: newIdx
+          });
         });
   }
 
   render () {
+    const PRIMARY = colors[this.state.idxColor].primary;
+    const SECOND = colors[this.state.idxColor].second;
+
     return (
-      <>
-        <h3> {this.state.quote} </h3>
-        <button onClick={this.newQuote}> Hola </button>
-      </>
+      <div className="div" style={{backgroundColor: PRIMARY}} >
+        <Card
+          color={SECOND}
+          newQuote={this.newQuote}
+          isquote={this.state.isquote}
+          quote={this.state.quote}
+        />
+      </div>
     )
   }
 }
 
 ReactDOM.render (
-  <div className="div"> <Card /> </div>,
+  <App />,
   document.getElementById('root')
 );
